f49e525ee1dc8b6ead8f63e325a6caec46a0da4a
Omar Ahmed O <deeyndiid@gmail.com>
2024-11-21 11:40:08 +0000

    Update task 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,13 +10,13 @@ class User(db.Model):
     """
     User class
     """
-    __tablename__ = 'users'  # Specify the table name
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, nullable=False)
-    hashed_password = db.Column(db.String, nullable=False)
-    session_id = db.Column(db.String, nullable=True)
-    reset_token = db.Column(db.String, nullable=True)
+    email = db.Column(db.String(250), nullable=False)
+    hashed_password = db.Column(db.String(250), nullable=False)
+    session_id = db.Column(db.String(250), nullable=True)
+    reset_token = db.Column(db.String(250), nullable=True)
 
     def __repr__(self):
         return f"<User id={self.id} email={self.email}>"
